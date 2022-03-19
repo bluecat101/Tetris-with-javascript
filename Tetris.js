7e5dd7ba5f66abbc944cb688365554271576d71a
bluecat101 <97800922+bluecat101@users.noreply.github.com>
2022-03-20 04:25:19 +0900

    start

---
+++ Tetris.js
@@ -0,0 +1,34 @@
+function start() {
+  console.log("a");
+  let i;
+  let status="on_play";
+
+  let can=canvas.getContext("2d");
+  let arrayTetro=[//[x1,y1,x2,y2,x3,y3,x4,y4]
+    [0,0,1,1,1,4,0,4]//I型
+  ]
+  // let Tetro= document.createElement
+  // for (i=0;i<10;++i){
+    can.fillStyle="black";
+    can.fillRect=(10,10,100,400);
+  // }
+can.fill;
+}
+
+
+
+
+console.log("a");
+  let i;
+  let status="on_play";
+
+  let can=canvas.getContext("2d");
+  let arrayTetro=[//[x1,y1,x2,y2,x3,y3,x4,y4]
+    [0,0,1,1,1,4,0,4]//I型
+  ]
+  // let Tetro= document.createElement
+  // for (i=0;i<10;++i){
+    can.fillStyle="black";
+    can.fillRect=(10,10,100,400);
+  // }
+can.fill;
